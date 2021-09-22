f955582b774f0859d5fe24014e28dbcb2602adcd
Akinyemi Taiwo k <karrim4real@gmail.com>
2021-09-22 15:57:03 +0800

    body-parser remove

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const userRoute = require("./routes/user");
 const mongoose = require("mongoose");
 // const ConnectDb = require("./config/keys");
